7c52148501b6f6dd1c8e9e1b9fc7700d48e1f575
unknown <2572496954@qq.com>
2022-04-01 13:43:04 +0800

    update style

--- src/components/RichText/lib.js
+++ src/components/RichText/lib.js
@@ -115,8 +115,8 @@ const _TL_LINE = [
     data: `<div class="fn-tl_li_2"><div class="m-lt"><em>目录/contents</em></div><div class="m-rt"></div></div>`,
   },
   {
-    key: 'tlti003',
-    data: `<div class="fn-tl_li_3"><div class="m-top"><div class="m-top-left"><em>惊 蛰</em></div><div class="m-top-right"><em> 2022.03.05 </em></div></div><div class="m-word"><li >时至惊蛰，阳气上升、气温回暖、春雷乍动、雨水增多，万物生机盎然。农耕生产与大自然的节律息息相关。</li><br><li >惊蛰节气在农耕上有着相当重要的意义，它是古代农耕文化对于自然节令的反映。</li></div></div>`,
+    key: 'tlli010',
+    data: `<div class='fn-tl_li_10'><div>交通安全日</div></div>`,
   },
 ];
 
@@ -127,8 +127,8 @@ const _TL_IMG = [
     data: `<div class='fn-tl_im_1'><img src=${tl_02} ><div class='m-lt'><em>所思日记</em></div><div class='m-rt'><div class='m-tp'><img src=${tl_01}><em>32℃</em></div><div class='m-bt'>2022年 3月18日</div></div></div>`,
   },
   {
-    key: 'tlim002',
-    data: `<div><div class="fn-tl_im_2"><div class="m-rt"><em>铭记历史</em></div><img class="m-cen" src=${img1} alt=""><div class="m-rt"><em>勿忘国耻</em></div></div></div>`,
+    key: 'tlim010',
+    data: `<div class='fn-tl_im_10'><div class='m-bd'>SALE</div></div>`,
   },
 ];
 
@@ -139,12 +139,8 @@ const _TL_BCOL = [
     data: `<div class='fn-tl_bc_1'><em>向雷锋同志学习</em><div><i></i><label>学雷锋纪念日</label><i></i></div></div>`,
   },
   {
-    Key: 'tlbc002',
-    data: `<div class="fn-tl_bc_2">
-                            <div class="m-cen">
-                              <em>聚焦两会 · 关注民生</em>
-                            </div>
-                          </div>`,
+    key: 'tlbc010',
+    data: `<div class='fn-tl_bc_10'><div class='m-tl'>HAPPY NEW YEAR</div><div class='m-bd'>05</div>`,
   },
 ];
 
@@ -155,16 +151,10 @@ const _TL_SYM = [
     data: `<div class='fn-tl_sy_1'><i></i><em>1</em><i></i></div>`,
   },
   {
-    key: 'tlsy002',
-    data: `<div class="fn-tl_sy_2"><div class="m-lt"><div class="m-lt-wd1">圣</div><div class="m-lt-wd2">诞</div><div class="m-lt-wd1">快</div><div class="m-lt-wd2">乐</div><div class="m-lt-wd3"><em>/ CHRISTMAS 12.25</em></div></div><div class="tl-sym-2-6"><div class="m-rt-tp"><div class="m-rt-tp-rt"></div><div class="m-rt-tp-cen"></div><div class="m-rt-tp-lt"></div></div><div class="m-rt-bt"><div class="m-rt-bt-lt"></div><div class="m-rt-bt-rt"></div></div></div></div>`,
-  },
-  {
-    key: 'tlsy003',
-    data: `<div class="fn-tl_sy_3"><div class="m-rt"></div><div class="m-cen"></div><div class="m-lt"><em>面对疫情 不必恐慌</em></div></div>`,
+    key: 'tlsy010',
+    data: `<div class='fn-tl_sy_10'><div class='m-bd'>今夜偏知春气暖</div></div>`,
   },
 ];
-
-// 安慰赋完分
 
 const _TL = [_TL_BASE, _TL_LINE, _TL_IMG, _TL_BCOL, _TL_SYM];
 
@@ -180,8 +170,8 @@ const _CD_BASE = [
     data: `<div class='fn-cd_bs_2'><label>请输入标题</label><em>但凡认真思考过自己的生活，想必谁都会希望活得更愉悦、美好。为了追求更好的品质、更美的东西而痛下功夫，这就是我们认为的真正意义上的时尚。</em></div>`,
   },
   {
-    key: 'cdbs003',
-    data: `<div class="fn-cd_bs_9"><div class="m-bt-tp"><em>三八妇女节</em></div><div class="m-bt-cen"></div><div class="m-bt-bt"></div><div class="m-tp"><em>国际妇女节是全世界许多国家都庆祝的节日。这一天，妇女们作出的成就得到肯定，无论她们的国籍、民族、语言、文化、经济状况和政治立场如何。自设立之初，国际妇女节为发达国家及发展中国家的妇女开启了一个新天地。</em></div></div>`,
+    key: 'cdbs010',
+    data: `<div class='fn-cd_bs_10'><div>请输入文字</div></div>`,
   },
 ];
 
@@ -193,8 +183,8 @@ const _CD_LINE = [
     <li>公安部在16个城市试点基础上，在全国分两批推广机动车检验标志电子化，为机动车所有人、驾驶人以及相关行业和管理部门提供电子证照服务。</li></div></div>`,
   },
   {
-    key: 'cdli002',
-    data: `<div class="fn-cd_li_2"><div class="m-tp"><div class="m-tp-lt"></div><div class="m-tp-rt"></div></div><div class="m-cen"><p>从最开始对新冠疫情的猝不及防，到全民抗疫的众志成城、共克时艰，再到实现疫情防控常态化。</p><br><p>从最开始对新冠疫情的猝不及防，到全民抗疫的众志成城、共克时艰，再到实现疫情防控常态化。</p></div><div class="m-bt"><div class="m-bt-lt"></div><div class="m-bt-rt"></div</div></div>`,
+    key: 'cdli010',
+    data: "<div class='fn-cd_li_10'><div class='m-bd'><li>两会是对自1959年以来历年召开的中华人民共和国全国人民代表大会和中国人民政治协商会议的统称。</li><li>由于两场会议会期基本重合，而且对于国家运作的重要程度都非常的高，故简称做“两会”。</li><li>两会是对自1959年以来历年召开的中华人民共和国全国人民代表大会和中国人民政治协商会议的统称。</li></div></div>",
   },
 ];
 
@@ -205,8 +195,15 @@ const _CD_BCOL = [
     data: `<div class='fn-cd_bc_1'><i>1</i><div>植树节是按照法律规定宣传保护树木，并组织动员群众积极参加以植树造林为活动内容的节日。提倡通过这种活动，激发人们爱林造林的热情、意识到环保的重要性。</div></div>`,
   },
   {
-    key: 'cdbc002',
-    data: `<div class="fn-cd_bc_2"><div class="m-tp"></div><div class="m-bt"><em>植树节是按照法律规定宣传保护树木，并组织动员群众积极参加以植树造林为活动内容的节日。提倡通过这种活动，激发人们爱林造林的热情、意识到环保的重要性。</em></div></div>`,
+    key: 'cdbc010',
+    data: `<div class='fn-cd_bc_10'>
+    <div class='m-tl'>
+    <label>项目 | 高山滑雪</label>
+    <div class='m-bd'><img src=${img2}></div>
+    <em>2015年07月31日17时57分，国际奥委会第128次全会在吉隆坡举行，投票选出2022年冬奥会举办城市。</em>
+    <em>2015年07月31日17时57分，国际奥委会第128次全会在吉隆坡举行，投票选出2022年冬奥会举办城市。</em>
+    </div>
+    </div>`,
   },
 ];
 
@@ -217,8 +214,8 @@ const _CD_PATT = [
     data: `<div class='fn-cd_bc_1 fn-cd_pa_1 '><i>1</i><div>植树节是按照法律规定宣传保护树木，并组织动员群众积极参加以植树造林为活动内容的节日。提倡通过这种活动，激发人们爱林造林的热情、意识到环保的重要性。</div></div>`,
   },
   {
-    key: 'cdpa002',
-    data: `<div class="fn-cd_pa_2"><div class="m-cen"><em>从最开始对新冠痘情的猝不及防，到全民抗疫的众志成城、共克时艰，从最开始对新冠疫情的蠡不及防，到全民抗疫的众志成城、共克时艰，再到实现疫情防控常态化，我们积累了丰富的抗疫经验，取得了来之不易的重大成果。</em></div></div>`,
+    key: 'cdpa010',
+    data: `<div class='fn-cd_pa_10 '><div><li>任教高中语文，目前担任XX教育“十人课堂”语文老师，深受学生、家长的喜爱。任教高中语文，目前担任XX教育“十人课堂”语文老师，深受学生、家长的喜爱。</li></div></div>`,
   },
 ];
 
@@ -229,8 +226,11 @@ const _CD_QUES = [
     data: `<div class='fn-cd_qu_1'><div class="m-q"><div>两会是指什么会议？</div></div><div class="m-a"><div>两会是对自1959年以来历年召开的中华人民共和国全国人民代表大会和中国人民政治协商会议的统称。<br>由于两场会议会期基本重合，而且对于国家运作的重要程度都非常的高，故简称做“两会”。</div></div></div>`,
   },
   {
-    key: 'cdqu002',
-    data: `<div class="fn-cd_qu_2"><div class="m-tp"><div class="m-tp-lt">聚焦</div><div class="m-tp-rt">两会是指什么会议？</div></div><div class="m-bt"><div class="m-bt-tp"><em>两会是对自1959年以来历年召开的中华人民共和国全国人民代表大会和中国人民政治协商会议的统称。</em></div><div class="m-bt-cen"><em>两会是对自1959年以来历年召开的中华人民共和国全国人民代表大会和中国人民政治协商会议的统称。</em></div><div class="m-bt-bt"><em>两会是对自1959年以来历年召开的中华人民共和国全国人民代表大会和中国人民政治协商会议的统称。</em></div></div></div>`,
+    key: 'cdqu010',
+    data: `<div class='fn-cd_qu_10'>
+    <div class='m-q'><div>面对疫情 不必恐慌</div></div>
+    <div class='m-a'><div><div>从最开始对新冠疫情的猝不及防，到全民抗疫的众志成城、共克时艰，再到实现疫情防控常态化，我们积累了丰富的抗疫经验。</div></div></div>
+</div>`,
   },
 ];
 
@@ -241,8 +241,8 @@ const _CD_AXIS = [
     data: `<div class='fn-cd_ax_1'><div class="m-ax"><div class="m-tl"><em>立春</em> <label>二十四节气之一</label></div><div class="m-bd"><div>立春，为二十四节气之首。立，是“开始”之意；春，代表着温暖、生长。<br>二十四节气最初是依据“斗转星移”制定，当北斗七星的斗柄指向寅位时为立春。</div></div></div><div class="m-ax"><div class="m-tl"><em>立春</em> <label>二十四节气之一</label></div><div class="m-bd"><div>立春，为二十四节气之首。立，是“开始”之意；春，代表着温暖、生长。<br>二十四节气最初是依据“斗转星移”制定，当北斗七星的斗柄指向寅位时为立春。</div></div></div><div class="m-ax"><div class="m-tl"><em>立春</em> <label>二十四节气之一</label></div><div class="m-bd"><div>立春，为二十四节气之首。立，是“开始”之意；春，代表着温暖、生长。<br>二十四节气最初是依据“斗转星移”制定，当北斗七星的斗柄指向寅位时为立春。</div></div></div></div>`,
   },
   {
-    key: 'cdax001',
-    data: `<div class="fn-cd_ax_2"><div class="m-tp"></div><div class="m-bt"></div><div><em> HAPPY NEW YEAR </em></div><div class="m-bt"></div><div class="m-tp"></div></div>`,
+    key: 'cdax010',
+    data: `<div class='fn-cd_ax_10'><div class="m-tl">01</div><div class='m-bd'>地球一小时是世界自然基金会应对全球气候变化所提出的一项全球性节能活动，提倡于每年三月的最后一个星期六当地时间晚上20:30。</div></div>`,
   },
 ];
 
@@ -252,7 +252,7 @@ const _CD = [_CD_BASE, _CD_LINE, _CD_BCOL, _CD_PATT, _CD_QUES, _CD_AXIS];
 // 基础图片
 const _IM_BASE = [
   { key: 'imbs001', data: `<div class='fn-im_bs_1'><img src=${img4} ></div>` },
-  { key: 'imbs002', data: `<div class='fn-im_bs_2'><img src=${img4} ></div>` },
+  { key: 'imbs010', data: `<div class='fn-im_bs_10'><img src=${img4} ></div>` },
 ];
 
 // 单图
@@ -261,11 +261,6 @@ const _IM_ONE = [
     key: 'imon001',
     data: `<div class='fn-im_on_1'><div class='fn-tl_li_1'><em>01</em></div><div class="m-img"><img src=${img4} ><em></em></div></div>`,
   },
-  {
-    key: 'imon002',
-    data: `<div class="fn-im_on_2"><div class="m-top"><div class="m-top-cir"></div><div class="m-top-cir"></div><div class="m-top-cir"></div><div class="m-top-cir"></div></div><div><img src=${img2}><div class="m-bt"></div></div></div>`,
-  },
-  { key: 'imon003', data: `<div class="fn-im_on_3"><img src=${img3} ></div>` },
 ];
 
 // 双图
@@ -273,10 +268,6 @@ const _IM_TWO = [
   {
     key: 'imtw001',
     data: `<div class='fn-im_tw_1'><div class='fn-tl_sy_1'><i></i><em>1</em><i></i></div><div class="m-bd"><div class="m-img"><img src=${img4} ></div><div class="m-img"><img src=${img4} ></div></div></div>`,
-  },
-  {
-    key: 'imtw002',
-    data: `<div class="fn-im_tw_2"><div class="m-tp"><img src=${img3}><img src=${img3}></div><div class="m-bt"><em>地球—小时是世界自然基金会应对全球气候变化所提出的—项全球性节能活动，提倡于每年三月的最后—个星期六当地时间晚上20:30。</em></div></div>`,
   },
 ];
 
@@ -286,7 +277,6 @@ const _IM_THR = [
     key: 'imth001',
     data: `<div class='fn-im_th_1'><em> 图标标题</em><div class="m-img"><img src=${img3} ></div><div class="m-img"><img src=${img3} ><img src=${img3} ></div></div>`,
   },
-  // {key:"imth001", data:`<div class="fn-im_th_2"><div class="m-lt"><img src=${img1}></div><div class="m-rt"><img class="tp" src=${img1}><img class="bt" src=${img1}></div></div>`},
 ];
 
 // 多图
@@ -323,8 +313,8 @@ const _LY_BASE = [
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
   },
   {
-    key: 'lybs002',
-    data: `<div class="fn-fr_bs_1"><div class="m-lt"></div><div class="m-rt"></div></div>`,
+    key: 'lybs010',
+    data: `<div class='fn-ly_ba_10'><div data='空白处输入文字'></div><div></div><div></div></div>`,
   },
 ];
 
@@ -334,6 +324,11 @@ const _LY_GROP = [
     key: 'lygr001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
   },
+
+  {
+    key: 'lygr010',
+    data: `<div class='fn-im_tw_1 fn-ly_gr_10'><div class="m-bd"><div class="m-img"><img src=${img4} ></div><div class="m-img"><img src=${img4} ></div></div><div class='m-ft'>地球一小时是世界自然基金会应对全球气候变化所提出的一项全球性节能活动，提倡于每年三月的最后一个星期六当地时间晚上20:30。</div></div>`,
+  },
 ];
 
 // 表格
@@ -341,10 +336,6 @@ const _LY_TAB = [
   {
     key: 'lyta001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
-  },
-  {
-    key: 'lyta002',
-    data: `<div class="fn-fr_tb_1"><table><tr class="m-tp"><td>时间</td><td>项目</td><td>内容</td></tr><tr class="m-bt"><td>8:10</td><td>滑雪</td><td>高山滑雪</td></tr><tr class="m-bt"><td>9:45</td><td>滑雪</td><td>越野滑雪</td></tr><tr class="m-bt"><td>9:45</td><td>滑雪</td><td>高山滑雪</td></tr><tr class="m-bt"><td>9:45</td><td>滑雪</td><td>越野滑雪</td></tr></table></div>`,
   },
 ];
 
@@ -382,6 +373,7 @@ const _CO_SPLL = [
     key: 'cosl001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
   },
+  { key: 'cosl010', data: `<div class='fn-co_sp_10'><div></div></div>` },
 ];
 
 // 分隔符
@@ -389,6 +381,10 @@ const _CO_SPLS = [
   {
     key: 'coss001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
+  },
+  {
+    key: 'coss010',
+    data: `<div class='fn-co_spls_10'><div><div><div></div></div>`,
   },
 ];
 
@@ -398,6 +394,7 @@ const _CO_STIC = [
     key: 'cosc001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
   },
+  { key: 'cosc010', data: `<div class='fn-co_st_10'><div>1</div></div>` },
 ];
 
 // SVG贴纸
@@ -413,6 +410,10 @@ const _CO_FOC = [
   {
     key: 'cofc001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
+  },
+  {
+    key: 'cofc010',
+    data: `<div class='fn-co_fo_10'><div><em>点击蓝字 关注我们</em><div></div>`,
   },
 ];
 
@@ -430,6 +431,10 @@ const _CO_QRCO = [
     key: 'coqr001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
   },
+  {
+    key: 'coqr010',
+    data: `<div class='fn-co_qr_10'><div class='m-tl'> <div>春</div><div>日</div><div></div><div>光</div><div>景</div></div><div class='m-bd'><img src=${img1}></div><div class='m-ft'><div><em>微信公众号</em><em>XIUMIUS</em></div><div><em>新浪微博</em><em>@秀米XIUMI</em></div></div></div>`,
+  },
 ];
 
 // 零件
@@ -438,6 +443,7 @@ const _CO_PART = [
     key: 'copa001',
     data: `<div class='fn-cd_bs_1'>内文的标题设计有两款，一款是细线搭配粗线的设计；另一款是同样的细线搭配“空心的粗线”。这两款线的设计都可以运用秀米的布局，可嵌套和可设置单边边框这两个特性就能做出来。本段文字设置了边距20像素。</div>`,
   },
+  { key: 'copa010', data: `<div class='fn-co_pa_10'><i>01</i></div>` },
 ];
 
 const _CO = [
